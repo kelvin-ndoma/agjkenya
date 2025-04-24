8f77a07b913cd32eff014a06158e58b7d0cebe31
Kelvin Ndoma <kelvinndomamutua@gmail.com>
2025-04-24 16:34:24 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from './Layouts/MainLayout'
+
+function App() {
+  return (
+    <MainLayout>
+      <AppRouter/>
+    </MainLayout>
+  )
+}
+
+export default App
---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function MainLayout() {
+  return (
+    <>
+     <Navbar/>
+    <div>MainLayout</div>
+    </>
+   
+  )
+}
+
+export default MainLayout
